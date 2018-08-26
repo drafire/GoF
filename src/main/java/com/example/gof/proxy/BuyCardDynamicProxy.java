fddf600dacd339937ec6499692c86eb2bc7b3075
drafire <hyljljlj@163.com>
2018-08-26 15:59:08 +0800

    主动生成了生成的.class文件，用于反编译后看到生成代码详情。更加深入第了解代理模式。反编译工具用：XJad

--- src/main/java/com/example/gof/proxy/BuyCardDynamicProxy.java
+++ src/main/java/com/example/gof/proxy/BuyCardDynamicProxy.java
@@ -10,6 +10,15 @@ public class BuyCardDynamicProxy implements InvocationHandler {
         this.object = object;
     }
 
+    /**
+     *  代理回调的方法，反编译后可以看到调用的是这段代码:super.h.invoke(this, m3, null);
+     *  m3 = Class.forName("com.example.gof.proxy.BuyCard").getMethod("buycard", new Class[0]);
+     * @param proxy 代理
+     * @param method  代理调用的而方法
+     * @param args 调用的方法的参数
+     * @return
+     * @throws Throwable
+     */
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
         System.out.println("动态代理，买车前");
--- src/main/java/com/example/gof/proxy/TestBuyCardDynamicProxy.java
+++ src/main/java/com/example/gof/proxy/TestBuyCardDynamicProxy.java
@@ -1,14 +1,27 @@
 package com.example.gof.proxy;
 
 import com.example.gof.proxy.impl.BuyCardImpl;
+import sun.misc.ProxyGenerator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.lang.reflect.Proxy;
 
 public class TestBuyCardDynamicProxy {
-    public static void main(String[] args) {
-        BuyCard buyCard=new BuyCardImpl();
-        BuyCard buyCardProxy= (BuyCard)Proxy.newProxyInstance(BuyCard.class.getClassLoader(),
+    public static void main(String[] args) throws IOException {
+        BuyCard buyCard = new BuyCardImpl();
+        BuyCard buyCardProxy = (BuyCard) Proxy.newProxyInstance(BuyCard.class.getClassLoader(),
                 new Class[]{BuyCard.class}, new BuyCardDynamicProxy(buyCard));
         buyCardProxy.buycard();
+
+        byte[] classFile= ProxyGenerator.generateProxyClass("TestProxy", new Class<?>[]{BuyCard.class});
+        File file=new File("D:\\java\\GoF\\src\\main\\java\\com\\example\\gof\\proxy/TestProxy.class");
+        FileOutputStream os=new FileOutputStream(file);
+        os.write(classFile);
+        os.flush();
+        os.close();
+
     }
 }
