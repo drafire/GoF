df2260628825feb52265d97c686b531acea98a21
pangtinghuan <pangtinghuan@tuandai.com>
2018-08-28 18:53:28 +0800

    增加了cglib代理时候，保存生成的文件

--- src/main/java/com/example/gof/proxy/TestBuyCardCglibProxy.java
+++ src/main/java/com/example/gof/proxy/TestBuyCardCglibProxy.java
@@ -1,9 +1,11 @@
 package com.example.gof.proxy;
 
 import com.example.gof.proxy.impl.BuyCardImpl;
+import org.springframework.cglib.core.DebuggingClassWriter;
 
 public class TestBuyCardCglibProxy {
     public static void main(String[] args) {
+        System.setProperty(DebuggingClassWriter.DEBUG_LOCATION_PROPERTY, TestBuyCardCglibProxy.class.getClass().getResource("/").getPath() );
         BuyCard buyCard=new BuyCardImpl();
         BuyCardCglibProxy proxy=new BuyCardCglibProxy();
         BuyCardImpl impl= (BuyCardImpl)proxy.getInstance(buyCard);
